e82c6ebacb28685a1a9eae524d5df27865239408
prashanthi-anam <113832925+prashanthi-anam@users.noreply.github.com>
2023-09-07 01:30:50 +0530

    Update eventhub.tf

--- eventhub.tf
+++ eventhub.tf
@@ -19,3 +19,70 @@ module "eventhub" {
 
 
 }
+resource "azurerm_monitor_action_group" "main" {
+  name                = "example-actiongroup"
+  resource_group_name = local.resource_group
+  short_name          = "exampleact"
+ email_receiver {
+    name                    = "email-receiver"
+    email_address           = "anamprashanthireddy@gmail.com"  # Replace with your email address
+    use_common_alert_schema = true
+  }
+}
+
+resource "azurerm_monitor_metric_alert" "example" {
+  name                = "example-metricalert"
+  resource_group_name = local.resource_group
+  scopes              = [azurerm_storage_account.appstore.id]
+  description         = "Action will be triggered when Transactions count is greater than 50."
+
+  criteria {
+    metric_namespace = "Microsoft.Storage/storageAccounts"
+    metric_name      = "Transactions"
+    aggregation      = "Total"
+    operator         = "GreaterThan"
+    threshold        = 1
+
+    dimension {
+      name     = "ApiName"
+      operator = "Include"
+      values   = ["appstore457768714567"]
+    }
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.main.id
+  }
+}
+resource "azurerm_monitor_action_group" "eventhub" {
+  name                = "eventhub-actiongroup"
+  resource_group_name = local.resource_group
+  short_name          = "exampleact"
+
+  email_receiver {
+    name                    = "email-receiver"
+    email_address           = "anamprashanthireddy@gmail.com"  # Replace with your email address
+    use_common_alert_schema = true
+  }
+}
+
+resource "azurerm_monitor_metric_alert" "eventhubalert" {
+  name                = "example-metriceventhubalert"
+  resource_group_name = local.resource_group
+  scopes              = [azurerm_eventhub_namespace.testeventhubnamespace.id]
+  description         = "Action will be triggered when Transactions count is greater than 50."
+
+  criteria {
+    metric_namespace = "Microsoft.EventHub/namespaces"
+    metric_name      = "ActiveConnections"
+    aggregation      = "Maximum" 
+    operator         = "GreaterThan"
+    threshold        = 1
+
+   
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.eventhub.id
+  }
+}
